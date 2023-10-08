9e56c131d030896a227b9872f32dfb5d8bee0c69
KPA6 <kpa6ullle@yandex.ru>
2023-10-08 08:08:18 +0600

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+
+app.get('/', (req, res) => {
+  res.send('Successful response.');
+});
+
+app.get('/api/fr', (req, res) => {
+    res.send('back and front are friends now');
+  });
+  
+  
+app.listen(3000, () => console.log('Example app is listening on port 3000.'));
