6ffbbf1abecc2731de65d00a660e19454b4712f3
KPA6 <kpa6ullle@yandex.ru>
2023-10-13 09:16:23 +0600

    +/models +/migrations

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
 const express = require('express');
-const { Sequelize, PrimaryKey, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('postgres://postgres:123@localhost:5432/postgres');
-try {
-  sequelize.authenticate()
-  console.log('Successfully connected to the database')
-} catch (e) {
-  console.log('Unable to connect to the database: ', e)
-}
-// Creating user module
-
-const User = sequelize.define('User', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-  },
-);
+const { sequelize } = require('./models');
 (async () => {
   // Creating database if it doesn't exist already
   await sequelize.sync({ force: false })
-  // Adding a new user in our database
- const john = await User.create({
-  name: 'John',
-  email: 'ex@ex.com',
-  password: '123',
-});
 })();
 const app = express();
 app.use((req, res, next) => {
